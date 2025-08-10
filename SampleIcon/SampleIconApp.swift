717bd3d84509da392f71ca17972be9fd9d44f704
Javier Calatrava <javi.calatrava@gmail.com>
2025-08-10 11:50:46 +0200

    Project finished

---
+++ SampleIcon/SampleIconApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleIconApp.swift
+//  SampleIcon
+//
+//  Created by JAVIER CALATRAVA LLAVERIA on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleIconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SampleIconTests/SampleIconTests.swift
@@ -0,0 +1,17 @@
+//
+//  SampleIconTests.swift
+//  SampleIconTests
+//
+//  Created by JAVIER CALATRAVA LLAVERIA on 2/8/25.
+//
+
+import Testing
+@testable import SampleIcon
+
+struct SampleIconTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
